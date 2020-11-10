789b8ff7a436300b8e1f0e685f8487b7f590c295
aizwellenstan <aizwellenstan@gmail.com>
2020-11-10 16:41:38 +0900

    v2-ShowCharts

--- harmonic.py
+++ harmonic.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('Data/JP225.csv')
 
-#data.columns = [['LocalTime', 'open', 'high', 'low', 'close', 'vol']]
+# data.columns = [['LocalTime', 'open', 'high', 'low', 'close', 'vol']]
 data.columns = [c.strip().lower().replace(' ','_') for c in data.columns]
 
 data = data.drop_duplicates(keep=False)
 
-#data.Date = pd.to_datetime(data.Date, format='%d.%m.%Y %H:%M:%S.%f')
+# data.Date = pd.to_datetime(data.Date, format='%d.%m.%Y %H:%M:%S.%f')
 
 data = data.set_index(data.local_time)
 
@@ -24,8 +24,21 @@ price = data.close.iloc[:100]
 
 # Fubd our relative extrema
 
-max_idx = argrelextrema(price.values, np.greater, order=1)
-min_idx = argrelextrema(price.values, np.less, order=1)
+### v1
+# max_idx = argrelextrema(price.values, np.greater, order=1)
+# min_idx = argrelextrema(price.values, np.less, order=1)
+# print(max_idx)
+# print(min_idx)
 
-print(max_idx)
-print(min_idx)
+max_idx = list(argrelextrema(price.values, np.greater, order=1)[0])
+min_idx = list(argrelextrema(price.values, np.less, order=1)[0])
+
+idx = max_idx+min_idx
+
+idx.sort()
+
+peaks = price.values[idx]
+
+plt.plot(price.values)
+plt.scatter(idx,peaks,c='r')
+plt.show()
